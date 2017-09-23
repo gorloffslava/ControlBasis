18a624ef0499b381dbf30506bd7a3a674efe5b1b
Gorloff <gorloff.slava@gmail.com>
2017-09-23 14:04:49 +0600

    2/1/6 v1 - без распознания жестов

--- ControlBasis/ViewController.swift
+++ ControlBasis/ViewController.swift
@@ -9,10 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var button: UIButton!
+    @IBOutlet weak var percentLabel: UILabel!
+    @IBOutlet weak var slider: UISlider!
 
+    @IBOutlet weak var amountLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.percentLabel.text = "\(self.slider.value.rounded())%"
+        self.amountLabel.text = "0$"
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +26,32 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func buttonTapped(_ sender: UIButton) {
+       let alert = UIAlertController(title: "Договор с Люцифером", message: "Вы хотите подписать договор на продажу собственной души?", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Подписать", style: .default) {(alertAction) in self.view.backgroundColor = #colorLiteral(red: 1, green: 0, blue: 0.009946824187, alpha: 1)}
+        let cancelAction = UIAlertAction(title: "Отменить", style: .cancel, handler: nil)
+        alert.addAction(action)
+        alert.addAction(cancelAction)
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    @IBAction func switchChanged(_ sender: UISwitch) {
+        self.button.isEnabled = sender.isOn
+    }
+    
+    @IBAction func sliderChanged(_ sender: UISlider) {
+        self.percentLabel.text = "\(sender.value.rounded())%"
+        
+    }
+    @IBAction func textChanged(_ sender: UITextField) {
+        if let summ = Double(sender.text!) {
+            self.amountLabel.text = "\((summ * Double(pow(self.slider.value / 100 + 1, 10))).rounded())$"
+        } else {
+            self.amountLabel.text = "Не могу рассчитать долг"
+        }
+    }
+    
 
+    
 }
 
