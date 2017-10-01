0de1c3c5542488a1d5b0dfb0e4fcb883e7c33963
Gorloff <gorloff.slava@gmail.com>
2017-10-01 17:56:36 +0600

    2/5/4

--- ControlBasis/ViewController.swift
+++ ControlBasis/ViewController.swift
@@ -14,11 +14,15 @@ class ViewController: UIViewController {
     @IBOutlet weak var slider: UISlider!
 
     @IBOutlet weak var amountLabel: UILabel!
+    @IBOutlet weak var switchControl: UISwitch!
     
+    @IBOutlet weak var textField: UITextField!
+
+
     @IBAction func respondToTapGesture(_ sender: UITapGestureRecognizer) {
-        let location = sender.location(in: self.view)
-        print(location)
+        self.textField.resignFirstResponder()
     }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         self.percentLabel.text = "\(self.slider.value.rounded())%"
@@ -32,11 +36,18 @@ class ViewController: UIViewController {
     }
 
     @IBAction func buttonTapped(_ sender: UIButton) {
-       let alert = UIAlertController(title: "Договор с Люцифером", message: "Вы хотите подписать договор на продажу собственной души?", preferredStyle: .alert)
+        let alert = UIAlertController(title: "Договор с Люцифером", message: "Вы хотите подписать договор на продажу собственной души?", preferredStyle: .actionSheet) //
         let action = UIAlertAction(title: "Подписать", style: .default) {(alertAction) in self.view.backgroundColor = #colorLiteral(red: 1, green: 0, blue: 0.009946824187, alpha: 1)}
         let cancelAction = UIAlertAction(title: "Отменить", style: .cancel, handler: nil)
         alert.addAction(action)
         alert.addAction(cancelAction)
+        
+        if let popoverController = alert.popoverPresentationController {
+            popoverController.sourceView = sender
+            popoverController.sourceRect = sender.bounds
+            popoverController.permittedArrowDirections = .down
+        }
+
         self.present(alert, animated: true, completion: nil)
     }
     
@@ -56,6 +67,10 @@ class ViewController: UIViewController {
         }
     }
     
+    @IBAction func segmentedTapped(_ sender: UISegmentedControl) {
+        self.switchControl.setOn((sender.selectedSegmentIndex == 0), animated: true)
+        self.perform(#selector(switchChanged(_:)), with: self.switchControl)
+    }
 
     
 }
